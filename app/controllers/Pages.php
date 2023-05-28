9bd890f2c3e8436fd600bcc94737d7a2a139a887
dragan002 <draganvujic29@gmail.com>
2023-05-28 22:45:02 +0200

    Creating a framework

---
+++ app/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+class Pages
+{
+    public function __construct()
+    {
+    }
+
+    public function about()
+    {
+        echo "this is about";
+    }
+    public function index()
+    {
+        echo "this is index";
+    }
+}
